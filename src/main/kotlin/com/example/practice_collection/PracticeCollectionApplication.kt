5c39608572aba28fd337ac5fdbf23421476a425d
ParkJiwoon <bcp0109@gmail.com>
2022-11-29 23:23:15 +0900

    🎉 Begin a project

---
+++ src/main/kotlin/com/example/practice_collection/PracticeCollectionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.practice_collection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeCollectionApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeCollectionApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/practice_collection/PracticeCollectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.practice_collection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeCollectionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
