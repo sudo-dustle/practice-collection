6db98361a702c556a9133af7b9afafe466d58b0d
ParkJiwoon <bcp0109@gmail.com>
2022-11-30 23:38:18 +0900

    ⚰️ JavaList 인터페이스에서 사용하지 않는 toString() 제거

--- src/main/kotlin/com/example/practice_collection/list/JavaList.java
+++ src/main/kotlin/com/example/practice_collection/list/JavaList.java
@@ -2,7 +2,6 @@ package com.example.practice_collection.list;
 
 public interface JavaList<E> {
     int size();
-    String toString();
     void add(E e);
     E get(int index);
     boolean contains(E e);
